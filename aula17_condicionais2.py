fbf497eaa09cedf880c14f9f0bd3c0c8567a4054
gabrielscmelo <gabrielscmelo@hotmail.com>
2026-06-16 19:41:30 -0300

    Importanto aulas 15 à 18 mais o settings.json

---
+++ aula17_condicionais2.py
@@ -0,0 +1,23 @@
+# if / elif      / else
+# se / se não se / se não
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('código para condição 1.')
+    print('código para condição 1.')
+elif condicao2:
+    print('código para condição 2')
+elif condicao3:
+    print('código para condição 3')
+elif condicao4:
+    print('código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
