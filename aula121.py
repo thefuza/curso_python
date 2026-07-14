f13b15234fdabaf6ae5ff7b965a7ba7cc6ab8a0e
gabrielscmelo <gabrielscmelo@hotmail.com>
2026-07-14 16:14:42 -0300

    importando aula 121 e 122  - POO

---
+++ aula121.py
@@ -0,0 +1,16 @@
+# Métodos em instâncias de classes Python
+# Hard coded - algo que foi escrito diretamente no código
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando...')
+
+fusca = Carro('Fusca')
+print(fusca.nome)
+fusca.acelerar()
+
+celta = Carro('Celta')
+print(celta.nome) 
+celta.acelerar()
