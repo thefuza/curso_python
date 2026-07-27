5f0efe9e45ae2e47bb3dab307742324bb53231ad
gabrielscmelo <gabrielscmelo@hotmail.com>
2026-07-27 15:39:23 -0300

    importando aulas

--- aula140_heranca_multipla.py
+++ aula140_heranca_multipla.py
@@ -26,3 +26,30 @@
 # Para saber a ordem de chamada dos métodos
 # Use o método de classe Classe.mro()
 # Ou o atributo __mro__ (Dunder - Double Underscore)
+class A:
+    ...
+
+    def quem_sou(self):
+        print('A')
+
+class B(A):
+    ...
+
+    def quem_sou(self):
+        print('B')
+
+class C(A):
+    ...
+
+    def quem_sou(self):
+        print('C')
+
+class D(B, C):
+    ...
+
+    # def quem_sou(self):
+    #     print('D')
+
+d = D()
+d.quem_sou()
+print(D.mro())
